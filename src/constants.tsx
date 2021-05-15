dd94d848ce5946b4a8919d96999b8f3b2a9774bc
Tom Davenport <tom@uplifter.ai>
2021-05-15 12:53:14 +0100

    fixed connection

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,1 +1,1 @@
-export const SOCKETURL = ["localhost", "127.0.0.1", ""].includes(window.location.hostname) ? "http://localhost:8000" : "http://tom-games.azurewebsites.net/"
+export const SOCKETURL = ["localhost", "127.0.0.1", ""].includes(window.location.hostname) ? "http://localhost:8000" : "https://tom-games.azurewebsites.net/"
--- src/context/socket2.js
+++ src/context/socket2.js
@@ -2,6 +2,6 @@ import {io} from "socket.io-client";
 import React from 'react';
 import { SOCKETURL } from '../constants'
 
-export const socket = io( SOCKETURL + '/categories');
+export const socket = io( SOCKETURL + 'categories');
 
 export const SocketContext = React.createContext(socket);
